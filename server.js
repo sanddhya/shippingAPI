520c847724af07cdf23a0ab652467eb3d1a36e81
Sandhya Ankush Shitole <Sandhya.Shitole@pb.com>
2018-05-02 15:50:40 +0530

    change

--- server.js
+++ server.js
@@ -2,4 +2,6 @@ var http = require('http');
 var app = require('./app');
 var port = 3001;
 var server = http.createServer(app);
-server.listen(port);
+server.listen(port, function () {
+    console.log("Server running on port:" + port);
+});
